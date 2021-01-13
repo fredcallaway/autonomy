6208444f383ee506e99c22d575bcf6a18e741244
fredcallaway <fredc@llaway.com>
2021-01-13 11:39:41 -0800

    importance sampling

--- model/model.jl
+++ model/model.jl
@@ -3,29 +3,8 @@ using Distributions
 using QuadGK
 using Parameters
 using Serialization
-
-abstract type SampleWeighter end
-
-function weight(sw::SampleWeighter, u, p)
-    x = score(sw, u, p)
-    for i in eachindex(x)
-        if x[i] < -.0001
-            serialize("tmp/bad", (sw, u, p))
-            error("Negative weight!")
-        else
-            x[i] = max(0., x[i])
-        end
-    end
-    x ./= sum(x)
-    x
-end
-
-subjective_value(sw::SampleWeighter, s, u, p) = mean(sample(u, Weights(weight(sw, u, p)), s; replace=false))
-objective_value(u, p, k) = maximum(sample(u, Weights(p), k; replace=false))
-
-choice_prob(sw::SampleWeighter, s, u, p) = N_MC \ mapreduce(+, 1:N_MC) do i
-    subjective_value(sw, s, u, p) > 0
-end
+include("utils.jl")
+# %% ==================== Environment ====================
 
 @with_kw struct Env
     loc::Float64 = 0.
@@ -46,68 +25,90 @@ function sample_problem(env::Env)::Problem
     (u, p)
 end
 
-function expected_value(env::Env)
-    @unpack loc, noise, k = env
-    emax(k, Normal(loc, noise))
-end
-
-function monte_carlo(f, N=10000)
-    N \ mapreduce(+, 1:N) do i
-        f()
-    end
-end
+sample_objective_value(u, p, k) = maximum(sample(u, Weights(p), k; replace=false))
 
 "Expected maximum of N samples from a Normal distribution"
-function emax(N::Int, d::Normal)
+function expected_maximum(N::Int, d::Normal)
     mcdf(x) = cdf(d, x)^N
     lo = d.μ - 10d.σ; hi = d.μ + 10d.σ
     - quadgk(mcdf, lo, 0, atol=1e-5)[1] + quadgk(x->1-mcdf(x), 0, hi, atol=1e-5)[1]
 end
 
-# choice_value(u, p, k) = N_MC \ mapreduce(+, 1:N_MC) do i
-#     objective_value(u, p, k)
-# end
+function expected_value(env::Env)
+    @unpack loc, noise, k = env
+    expected_maximum(k, Normal(loc, noise))
+end
 
-# function sample_value(n, k, s; αs=0:0.1:1)
-#     u, p = sample_problem(n)
-#     val = choice_value(u, p, k)
-#     prob = [choice_prob(sw, s, u, p) for sw in αs]
-#     prob .* val
-# end
 
 # %% ==================== Sampling strategies ====================
 
+abstract type Sampler end
 
-struct Multiplicative <: SampleWeighter
+struct Multiplicative <: Sampler
     β_p::Real
     β_u::Real
 end
 
-function score(sw::Multiplicative, u, p)
-    @. u ^ sw.β_u * p ^ sw.β_p
+function score(sampler::Multiplicative, u, p)
+    @. u ^ sampler.β_u * p ^ sampler.β_p
 end
 
-struct Softmax <: SampleWeighter
+struct Softmax <: Sampler
     β_p::Real
     β_u::Real
 end
 
-function score(sw::Softmax, u, p)
-    @. p ^ sw.β_p * exp(u * sw.β_u)
+function score(sampler::Softmax, u, p)
+    @. p ^ sampler.β_p * exp(u * sampler.β_u)
 end
 
-struct AbsExp <: SampleWeighter
+struct AbsExp <: Sampler
     β_abs::Real
     β_exp::Real
 end
 
-function score(sw::AbsExp, u, p)
-    if sw.β_abs == sw.β_exp == 0
+function score(sampler::AbsExp, u, p)
+    if sampler.β_abs == sampler.β_exp == 0
         return p
     else
-        @. p * (sw.β_abs * abs(u) + sw.β_exp * exp(u))
+        @. p * (sampler.β_abs * abs(u) + sampler.β_exp * exp(u))
     end
 end
+
+
+# %% ==================== Evaluation ====================
+
+struct Evaluator{S<:Sampler}
+    sampler::S
+    importance::Float64
+end
+
+function subjective_value(evaluator::Evaluator, s, u, p)
+    w = weight(evaluator.sampler, u, p)
+    samples = if evaluator.importance > 0  # reweighting
+        idx = sample(eachindex(u), Weights(w), s; replace=false)
+        @. u[idx] * (p[idx] / w[idx]) ^ evaluator.importance
+    else
+        sample(u, Weights(w), s; replace=false)
+    end
+    mean(samples)
+end
+
+function weight(sampler::Sampler, u, p)
+    x = score(sampler, u, p)
+    # x .= max.(0., x)
+    # for i in eachindex(x)
+    #     if x[i] < -.0001
+    #         serialize("tmp/bad", (sampler, u, p))
+    #         error("Negative weight!")
+    #     else
+    #         x[i] = max(0., x[i])
+    #     end
+    # end
+    x ./= sum(x)
+    x
+end
+
 
 # %% ==================== Objective ====================
 
@@ -123,16 +124,16 @@ function Objective(env, n_problem=1000)
     end
     true_vals = pmap(problems) do (u, p)
         monte_carlo(10000) do
-            objective_value(u, p, env.k)
+            sample_objective_value(u, p, env.k)
         end
     end
     Objective(env, problems, true_vals)
 end
 
-function (f::Objective)(sw::SampleWeighter, map=map)
+function (f::Objective)(evaluator::Evaluator, map=map)
     choice_probs = map(f.problems) do (u, p)
         monte_carlo(1000) do 
-            subjective_value(sw, f.env.s, u, p) > 0
+            subjective_value(evaluator, f.env.s, u, p) > 0
         end
     end
     mean(choice_probs .* f.true_vals)
--- model/utils.jl
+++ model/utils.jl
@@ -17,3 +17,16 @@ end
 function keyed(name, xs)
     KeyedArray(xs; Dict(name => xs)...)
 end
+
+function monte_carlo(f, N=10000)
+    N \ mapreduce(+, 1:N) do i
+        f()
+    end
+end
+
+"Expected maximum of N samples from a Normal distribution"
+function expected_maximum(N::Int, d::Normal)
+    mcdf(x) = cdf(d, x)^N
+    lo = d.μ - 10d.σ; hi = d.μ + 10d.σ
+    - quadgk(mcdf, lo, 0, atol=1e-5)[1] + quadgk(x->1-mcdf(x), 0, hi, atol=1e-5)[1]
+end
