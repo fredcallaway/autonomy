65c6618a3df33993544a31be8e4990f5831551d5
fredcallaway <fredc@llaway.com>
2021-01-16 08:55:55 -0800

    move plotting utils into figure

--- model/figure.jl
+++ model/figure.jl
@@ -15,3 +15,22 @@ function figure(f, name="tmp"; kws...)
         cp(p, "figs/$name.png"; force=true)
     end
 end
+
+function Plots.heatmap(X::KeyedArray{<:Real,2}; kws...)
+    ylabel, xlabel = dimnames(X)
+    heatmap(reverse(axiskeys(X))..., X; xlabel, ylabel, kws...)
+end
+
+function Plots.plot(x::KeyedArray{<:Real,1}; kws...)
+    plot(axiskeys(x, 1), collect(x); xlabel=string(dimnames(x, 1)), kws...)
+end
+
+function Plots.plot(X::KeyedArray{<:Real,2}; kws...)
+    k = dimnames(X, 2)
+    plot(axiskeys(X, 1), collect(X);
+        xlabel=dimnames(X, 1),
+        label=reshape(["$k=$v" for v in axiskeys(X, 2)], 1, :),
+        palette=collect(cgrad(:viridis, size(X, 2), categorical = true)),
+        kws...
+    )
+end
